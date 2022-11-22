ec262ce4e331a484f56b63f6b7913f3f9a0ec56a
Takahashi Kei <kei-ta@ws21is099.naist.jp>
2022-11-22 16:04:12 +0900

    first commit

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -5,23 +5,23 @@ from tests.csv_test import CSVPrinter
 class TestCSVPrinter(unittest.TestCase):
 
     def test_read(self):
-        printer = CSVPrinter("test.csv")
+        printer = CSVPrinter("tests/test.csv")
         l = printer.read()
         self.assertEqual(2, len(l))
 
     def test_read1(self):
-        printer = CSVPrinter("test.csv")
+        printer = CSVPrinter("tests/test.csv")
         line = printer.read()
         self.assertEqual(3, len(line))
 
     def test_read2(self):
-        printer = CSVPrinter("test.csv")
+        printer = CSVPrinter("tests/test.csv")
         line = printer.read()
         self.assertEqual("value2B", line[1][1])
 
     def test_read3(self):
         try:
-            printer = CSVPrinter("test.csv")
+            printer = CSVPrinter("tests/test.csv")
             printer.read()
             unittest.TestCase.fail("This line should not be invoked")
         except FileExistsError as e:
