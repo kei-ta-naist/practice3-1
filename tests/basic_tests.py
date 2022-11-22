82baf276de3be1b3075c471a430eee0b3160446e
Takahashi Kei <kei-ta@ws21is099.naist.jp>
2022-11-22 16:08:39 +0900

    first commit

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -4,10 +4,10 @@ from tests.csv_test import CSVPrinter
 
 class TestCSVPrinter(unittest.TestCase):
 
-    def test_read(self):
-        printer = CSVPrinter("tests/test.csv")
-        l = printer.read()
-        self.assertEqual(2, len(l))
+    # def test_read(self):
+    #     printer = CSVPrinter("tests/test.csv")
+    #     l = printer.read()
+    #     self.assertEqual(3, len(l))
 
     def test_read1(self):
         printer = CSVPrinter("tests/test.csv")
@@ -17,7 +17,7 @@ class TestCSVPrinter(unittest.TestCase):
     def test_read2(self):
         printer = CSVPrinter("tests/test.csv")
         line = printer.read()
-        self.assertEqual("value2B", line[1][1])
+        self.assertEqual("value2B", line[1][2])
 
     def test_read3(self):
         try:
